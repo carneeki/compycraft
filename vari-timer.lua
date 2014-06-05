d0625363a8e0b43822c21d9c753a88e173625f23
Adam Carmichael <carneeki@carneeki.net>
2014-06-05 20:46:05 +1000

    added vari-timer

---
+++ vari-timer.lua
@@ -0,0 +1,19 @@
+local runtime=5
+term.clear()
+term.setCursorPos(1,1)
+redstone.setOutput("top", true) -- lamp off (inverted)
+redstone.setOutput("back", false) -- motor off
+
+while true do
+  write("Runtime (seconds): ")
+  runtime = tonumber(read())
+  redstone.setOutput("top", false) -- lamp on
+  redstone.setOutput("back", true) -- motor on
+
+  for i=1,runtime do
+    write(runtime-i .. "s remaining...\n")
+    sleep(1)
+  end
+  redstone.setOutput("top", true) -- lamp off
+  redstone.setOutput("back", false) -- motor off
+end
